982229f78a77320a418371ac564e4ba8df1978fa
Eduardo-Fogaca <97850417+Eduardo-Fogaca@users.noreply.github.com>
2022-01-30 18:52:06 -0300

    Add files via upload

---
+++ Aula Python/exercicio13Aula3.py
@@ -0,0 +1,3 @@
+dict_produtos_valores = {"bacon":7.50,"alface":2.80,"pão":8.20}
+
+print(dict_produtos_valores)
---
+++ Aula Python/exercicio14Aula3.py
@@ -0,0 +1,5 @@
+dict_produtos_valores = {"bacon":7.50,"alface":2.80,"pão":8.20}
+
+soma=dict_produtos_valores["bacon"]+dict_produtos_valores["alface"]+dict_produtos_valores["pão"]
+
+print(str(soma))
